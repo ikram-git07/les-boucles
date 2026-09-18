df7d12d1ff617379050e8ed9676a8f25bb2872d7
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-18 11:53:46 +0100

    boucle2.js

---
+++ boucle2.js
@@ -0,0 +1,7 @@
+const prompt =require("prompt-sync")();
+let n = parseInt(prompt("entrez un nombre entier pour un factorielle :"));
+let factorille = 1;
+for (let i = 1; i <= n; i++) {
+ factorille *= i  ; 
+}
+        console.log (`la factorielle de ${n} est: ${factorille}`);
