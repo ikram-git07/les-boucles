2ec75a510ee115e7252daa26e2e8086e987a9550
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-19 14:29:41 +0100

    tab3.js

---
+++ tab3.js
@@ -0,0 +1,31 @@
+ const prompt =require("prompt-sync")();
+ const nbr = Number (prompt("entrez le nombre d'éléments d'un tableau "));
+ const list = [];
+ for ( let i = 0; i < nbr; i++ ){
+ list[i]= Number (prompt ("entrez éléments du tableaus "));
+ console.log (list);
+ }
+
+
+// let somme = 0 ;
+//  for (let i = 0 ; i < list.length; i++ ){
+//     somme = somme + list[i] ;
+// }
+// console.log(somme);
+
+
+
+
+
+
+
+
+
+
+let somme = 0 ;
+for (let i = 0; i < list.length; i++ ){
+somme = somme + list[i]; 
+}
+console.log (somme);
+
+
