0f542edbaf38a69fbf7b9aa2edd0780751a5a982
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-21 10:38:21 +0000

    chaine3.js

---
+++ chaine3.js
@@ -0,0 +1,5 @@
+//  lit deux chaînes de caractères et les concatène en une seule chaîne. Affichez la chaîne résultante.
+const prompt = require ("prompt-sync")();
+let ikram = prompt ("taper un caractere");
+let arbaoui = prompt ("taper le deuxeme caractere");
+console.log ("les deux est : "  + ikram + " " + arbaoui );
