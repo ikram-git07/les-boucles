b967e7004839501d2e9684b05ca03246fc36909c
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-21 11:57:35 +0000

    chaine5.js

---
+++ chaine5.js
@@ -0,0 +1,8 @@
+// qui lit une chaîne de caractères et affiche la chaîne inversée. Par exemple, si la chaîne est "abcd", le programme doit afficher "dcba".
+const prompt = require ("prompt-sync")();
+let chain = prompt ("taper un caractere");
+let inverse = "";
+for (let i = chain.length-1; i >= 0; i--  ){
+    inverse = inverse + chain[i]
+}
+console.log(inverse )
