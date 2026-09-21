22b60ea04979707be68daa9a079368da94be1003
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-21 11:14:15 +0000

    chaine4.js

---
+++ chaine4.js
@@ -0,0 +1,13 @@
+// qui lit deux chaînes de caractères et compare si elles sont égales ou non. Affichez un message indiquant si les chaînes sont égales ou différentes.
+const prompt = require ("prompt-sync")();
+let car1 = prompt ("taper un caractere");
+let car2 = prompt ("taper le deuxeme caractere");
+if ( car1 === car2 ){
+console.log ("les chaines sont : egaux");
+    }
+    else
+        console.log ("les chaines ne sont pas egaux");
+
+
+    
+
