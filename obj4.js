cb06c4be4ec4d2eb0c4fd7b1087b466341d173b8
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-22 10:21:45 +0000

    obj4.js

---
+++ obj4.js
@@ -0,0 +1,15 @@
+// qui crée un objet pour représenter un point dans un plan avec les propriétés x et y.
+//  Modifiez directement les valeurs de ses propriétés, 
+// puis affichez les valeurs du point.
+const  plan ={
+     x :14,  
+     y :40,
+    
+}
+plan.x = 10
+plan.y = 30
+console.log("les valeurs du pointest :");
+for (let cle in plan ){
+    console.log(cle ,plan[cle])
+}
+// console.log("les valeurs du pointest :");
