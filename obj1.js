8de016f5346c2d5d1982a7e2ee535821f2e59a17
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-22 09:35:14 +0000

    obj.1.js

---
+++ obj1.js
@@ -0,0 +1,9 @@
+const personne ={
+    nom :"ikram",
+    prenom :"arbaoui",
+    age :19,
+
+}
+console.log(personne.nom);
+console.log(personne.prenom);
+console.log(personne.age);
--- tab4.js
+++ tab4.js
@@ -1,22 +1,26 @@
 const prompt =require("prompt-sync")();
 const nbr = Number (prompt("entrez le nombre d'éléments d'un tableaus "));
 const list = [];
-for ( let i = 0; i < nbr; i++ ){
+let max = 
+for ( let i = 1; i < nbr.length; i++ ){
+    if (nbr[i] > max ){
+        max = nbr[i];
+    }
 list[i]= Number (prompt ("entrez éléments du tableaus "));
+}
 console.log (list);
-}
 
-let max  = list [0];
-if ( max < list[1] ){
-    max = list[1]
-}
-if ( max < list[2]){
-    max = list[2]
-}
-if ( max < list[3]){
-    max = list[3]
-}
-if ( max < list[4]){
-    max = list[4]
-}
+// let max  = list [0];
+// if ( max < list[1] ){
+//     max = list[1]
+// }
+// if ( max < list[2]){
+//     max = list[2]
+// }
+// if ( max < list[3]){
+//     max = list[3]
+// }
+// if ( max < list[4]){
+//     max = list[4]
+// }
 console.log (max);
