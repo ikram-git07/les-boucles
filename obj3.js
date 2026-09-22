08eba4c89ecd63201c6fe2149eb286e178c4c913
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-22 10:44:35 +0000

    obj3.js

---
+++ obj3.js
@@ -0,0 +1,8 @@
+// qui crée un objet pour représenter un rectangle avec les propriétés longueur et largeur. 
+// Écrivez une fonction qui prend cet objet en argument et calcule l'aire du rectangle.
+//  Affichez l'aire.
+const rectangle ={
+   longueur  :20,
+   largeur :13,
+}
+console.log("l'aire du rectangle est :" ,rectangle.longueur * rectangle.largeur );
