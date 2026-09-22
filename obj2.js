24c7874c4271381210ab3028e64e038acaebe877
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-22 09:53:24 +0000

    obj2.js

---
+++ obj2.js
@@ -0,0 +1,15 @@
+// qui crée un objet pour représenter un étudiant avec les propriétés nom, prenom et un tableau de notes. 
+// Attribuez des valeurs aux propriétés et aux notes,
+//  puis affichez les informations de l'étudiant.
+const étudiant ={
+    nom :"ikram",
+    prenom :"arbaoui",
+    notes : [16, 12, 10, 20 ],
+}
+console.log("voila tes info");
+console.log(étudiant.nom);
+console.log(étudiant.prenom);
+console.log(étudiant.notes);
+console.log(étudiant.notes[2]);
+console.log(étudiant.notes.length);
+
